de8165ef4b34e845202a4e07fb5a5bab5d9b64e0
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-04-20 16:54:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const computer_number = Math.floor(Math.random() * 5 + 1);
+const result = await inquirer.prompt([
+    {
+        name: "guessing_game",
+        type: "number",
+        message: "please guess a number between 1-5: ",
+    },
+]);
+// condition
+if (result.guessing_game === computer_number) {
+    console.log("Congratulation! you guess a right number");
+}
+else {
+    console.log("Wrong nuber,Try Again");
+}
